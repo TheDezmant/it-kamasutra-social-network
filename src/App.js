7be09934e944fe3b094110be2d5b51387dcc5f32
Mikhail.Ryazanov <adon1999@mail.ru>
2020-11-18 22:44:25 +0300

    deleted console.log test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from "./components/Header/index";
 import Content from "./components/Content";
 
 const App = () => {
-    console.log("test");
     return (
         <>
             <Header />
