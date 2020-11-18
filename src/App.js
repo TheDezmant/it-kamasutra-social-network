a186c56e0ef631c9c9cb5b2c19144dd8d86608a3
Mikhail.Ryazanov <adon1999@mail.ru>
2020-11-18 22:39:47 +0300

    added console.log test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/index";
 import Content from "./components/Content";
 
 const App = () => {
+    console.log("test");
     return (
         <>
             <Header />
