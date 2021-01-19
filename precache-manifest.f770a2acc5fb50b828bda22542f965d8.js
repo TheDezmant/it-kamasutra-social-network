c11883de53bba049c21037ccf491763442e4bc8d
Michail Ryazanov <adon1999@mai.ru>
2021-01-19 14:29:15 +0300

    Updates

---
+++ precache-manifest.f770a2acc5fb50b828bda22542f965d8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3571fc4f5cff547c3b4537e117c020",
+    "url": "/it-kamasutra-social-network/index.html"
+  },
+  {
+    "revision": "974a91629043d099a69e",
+    "url": "/it-kamasutra-social-network/static/css/main.2bf20ad9.chunk.css"
+  },
+  {
+    "revision": "de7a0212f4da5a81a4ed",
+    "url": "/it-kamasutra-social-network/static/js/2.405898d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/it-kamasutra-social-network/static/js/2.405898d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f1157313319e336010b",
+    "url": "/it-kamasutra-social-network/static/js/3.1bbfaa62.chunk.js"
+  },
+  {
+    "revision": "bba2033549483a1fc219",
+    "url": "/it-kamasutra-social-network/static/js/4.dcec87ce.chunk.js"
+  },
+  {
+    "revision": "974a91629043d099a69e",
+    "url": "/it-kamasutra-social-network/static/js/main.763935cf.chunk.js"
+  },
+  {
+    "revision": "b239efeb9985a871e9af",
+    "url": "/it-kamasutra-social-network/static/js/runtime-main.2bfa0cd3.js"
+  },
+  {
+    "revision": "6f99ea5782a2fbb58946a65f4552e697",
+    "url": "/it-kamasutra-social-network/static/media/user.6f99ea57.png"
+  }
+]);
